6f91d4b0a07b901b6b40bc99043856cf4efa380d
Abhishek Mandal <official.abhishek.mandal@gmail.com>
2025-09-23 11:03:32 +0530

    V3

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, GitHub Actions + Docker + Python v2!"}
+    return {"message": "Hello, GitHub Actions + Docker + Python v3!"}
